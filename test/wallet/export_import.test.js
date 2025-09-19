8c5113ad2b8a20da0273bb81e9f7dc3c4edc77e2
leonardostsouza <leonardo.souza@trac.network>
2025-09-19 12:26:35 +0200

    Allow empty passwords for keypair encryption

--- test/wallet/export_import.test.js
+++ test/wallet/export_import.test.js
@@ -32,6 +32,26 @@ test('PeerWallet: export and import preserves keypair', async t => {
     fs.unlinkSync(filePath);
 });
 
+test('PeerWallet: password can be empty', async t => {
+    const derivationPath = "m/44'/0'/0'/0'/0'";
+    const wallet = new PeerWallet({ mnemonic, derivationPath, networkPrefix });
+    await wallet.ready;
+    const emptyPassword = b4a.alloc(0);
+    wallet.exportToFile(filePath, emptyPassword);
+
+    const importedWallet = new PeerWallet({ networkPrefix });
+    await importedWallet.ready;
+    importedWallet.importFromFile(filePath, emptyPassword);
+
+    t.ok(b4a.equals(wallet.publicKey, importedWallet.publicKey));
+    t.ok(b4a.equals(wallet.secretKey, importedWallet.secretKey));
+    t.is(wallet.mnemonic, importedWallet.mnemonic);
+    t.is(wallet.address, importedWallet.address);
+    t.is(wallet.derivationPath, derivationPath);
+    t.is(wallet.derivationPath, importedWallet.derivationPath);
+    fs.unlinkSync(filePath);
+});
+
 test('PeerWallet: export throws if no secret key', async t => {
     const wallet = new PeerWallet({});
     await wallet.ready;
@@ -92,7 +112,7 @@ test('PeerWallet: import throws if password is not a buffer', async t => {
         importedWallet.importFromFile(filePath, 'notabuffer');
         t.fail('Expected error not thrown');
     } catch (error) {
-        t.is(error.message, 'Password must be a buffer with length greater than 0');
+        t.is(error.message, 'Password must be a buffer');
     }
     fs.unlinkSync(filePath);
 });
