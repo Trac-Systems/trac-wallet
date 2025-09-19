e8dbd75944076243313e5edc4f276a41ad106a80
leonardostsouza <leonardo.souza@trac.network>
2025-09-19 11:42:55 +0200

    Expose trac-crypto-api constants

--- constants.js
+++ constants.js
@@ -1,1 +1,6 @@
+import tracCryptoApi from 'trac-crypto-api';
+
 export const TRAC_NETWORK_MSB_MAINNET_PREFIX = 'trac';
+export const TRAC_PUBLIC_KEY_SIZE = tracCryptoApi.address.PUBLIC_KEY_SIZE;
+export const TRAC_SECRET_KEY_SIZE = tracCryptoApi.address.SECRET_KEY_SIZE;
+export const TRAC_SIGNATURE_SIZE = tracCryptoApi.signature.SIZE;
