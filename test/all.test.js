2ae68ebacfcca1667ac1dd5837520cce2cbc790f
leonardostsouza <leonardo.souza@trac.network>
2025-10-24 12:07:11 +0200

    Expose blake3 methods

--- test/all.test.js
+++ test/all.test.js
@@ -5,6 +5,7 @@ async function runTests() {
     await import('./address/address.test.js');
     await import('./wallet/sign_verify.test.js');
     await import('./wallet/export_import.test.js');
+    await import('./wallet/api_exposed.test.js');
     test.resume();
 }
 
