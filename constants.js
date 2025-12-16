164526c45e8ccac2837a33122eecac980ee69703
leonardostsouza <leonardo.souza@trac.network>
2025-12-16 10:57:01 +0100

    Fix constants naming

--- constants.js
+++ constants.js
@@ -1,6 +1,6 @@
 import tracCryptoApi from 'trac-crypto-api';
 
 export const TRAC_NETWORK_MSB_MAINNET_PREFIX = 'trac';
-export const TRAC_PUBLIC_KEY_SIZE = tracCryptoApi.address.PUBLIC_KEY_SIZE;
-export const TRAC_SECRET_KEY_SIZE = tracCryptoApi.address.SECRET_KEY_SIZE;
+export const TRAC_PUBLIC_KEY_SIZE = tracCryptoApi.address.PUB_KEY_SIZE;
+export const TRAC_SECRET_KEY_SIZE = tracCryptoApi.address.PRIV_KEY_SIZE;
 export const TRAC_SIGNATURE_SIZE = tracCryptoApi.signature.SIZE;
